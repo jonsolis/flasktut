5666f6704de25a3cb06e51e4ebc7fd9bc0edec98
js <jonathan.aaron.solis@gmail.com>
2015-09-17 20:48:27 -0400

    initial commit

---
+++ content_management.py
@@ -0,0 +1,8 @@
+
+def Content():
+	TOPIC_DICT = {"Basics":[["chillin' like a villain", "/chillin/"],
+			["such mad", "/such mad/"],
+			["gawddood", "/gawddood/"]],	
+		"Web Dev":[]}	
+
+	return TOPIC_DICT
